4bbd5e27088a537ac906b1626170f8211a2120d6
ianyshuang <ianyshuang@gmail.com>
2020-10-24 16:53:35 +0800

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const passport = require('./config/passport/passport')
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(passport.initialize())
 app.use('/', require('./routes/index'))
