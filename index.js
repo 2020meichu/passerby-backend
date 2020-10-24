4e9ebc67895fd345062901cbe24b3dc4b0fd2217
ianyshuang <ianyshuang@gmail.com>
2020-10-24 20:37:10 +0800

    change cors order

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const passport = require('./config/passport/passport')
 
+app.options('*', cors())
+app.use(cors())
+
 app.use((req, res, next) => {
   if (req.headers['x-api-key'] !== process.env.X_API_KEY) {
     return res.status(401).send('Unauthorized')
   }
   next()
 })
-app.use(cors())
-app.options('*', cors())
+
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(passport.initialize())
